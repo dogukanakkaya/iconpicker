ca4c35fe520d5fc0580aab39f52c40d1a9735b7c
dogukan <doguakkaya27@hotmail.com>
2021-07-08 22:56:16 +0300

    icons moved to json files, unnecessary codes removed

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/iconpicker.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'iconpicker.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
+    ],
+  },
+};
